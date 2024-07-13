501118d67d5ae09981797ff65c47bde07e94af28
Gustavo Neri <gnb.neri@gmail.com>
2024-07-13 11:47:53 -0300

    Initial commit

---
+++ src/components/MeuBotao.vue
@@ -0,0 +1,29 @@
+<!-- src/components/MeuBotao.vue -->
+<template>
+    <button @click="handleClick">{{ label }}</button>
+  </template>
+  
+  <script setup>
+  import { defineProps, defineEmits } from 'vue';
+  
+  const props = defineProps({
+    label: String,
+  });
+  
+  const emit = defineEmits(['click']);
+  
+  const handleClick = () => {
+    emit('click');
+  };
+  </script>
+  
+  <style scoped>
+  button {
+    background-color: #42b983;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+  }
+  </style>
+  
